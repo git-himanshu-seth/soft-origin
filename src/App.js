302cc719807ba4709b63fe720b5aa2bc11c9774a
Himanshu Seth <hseth879157@gmail.com>
2022-12-21 18:28:27 +0530

    create page for soft_origin animation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import React, { useState } from "react";
-import { render } from "react-dom";
-import { useTrail, animated as a } from "react-spring";
+import SoPage from './pages/page1/index'
+
+// import { render } from "react-dom";
+// import { useTrail, animated as a } from "react-spring";
 import "./App.css";
 
-const items = ["Soft", "Origin"];
-const config = { mass: 9, tension: 100, friction: 400 };
+// const items = ["Soft", "Origin"];
+// const config = { mass: 9, tension: 100, friction: 400 };
 
 function App() {
-  const [toggle, set] = useState(true);
-  const trail = useTrail(items.length, {
-    config,
-    opacity: toggle ? 1 : 0,
-    Y: toggle ? 0 : 0,
-    height: toggle ? 300 : 0,
-    from: { opacity: 0, Y: 20, height: 0 }
-  });
+//   const [toggle, set] = useState(true);
+//   const trail = useTrail(items.length, {
+//     config,
+//     opacity: toggle ? 1 : 0,
+//     Y: toggle ? 0 : 0,
+//     height: toggle ? 300 : 0,
+//     from: { opacity: 0, Y: 20, height: 0 }
+//   });
 
   return (
-    <div
-      className="trails-main"
-      onClick={() => set((state) => !state)}
-    >
-      <div>
-        {trail.map(({ Y, height, ...rest }, index) => (
-          <a.div
-            key={items[index]}
-            className="trails-text"
-            style={{
-              ...rest,
-              transform: Y.interpolate((Y) => `translate3d(0,${Y}px,0)`)
-            }}
-          >
-            <a.div
-              style={{ height, textTransform: "capitalize" }}
-            >
-              {items[index]}
-            </a.div>
-          </a.div>
-        ))}
-      </div>
-    </div>
+    <>
+    <SoPage/>
+    </>
+    // <div
+    //   className="trails-main"
+    //   onClick={() => set((state) => !state)}
+    // >
+    //   <div>
+    //     {trail.map(({ Y, height, ...rest }, index) => (
+    //       <a.div
+    //         key={items[index]}
+    //         className="trails-text"
+    //         style={{
+    //           ...rest,
+    //           transform: Y.interpolate((Y) => `translate3d(0,${Y}px,0)`)
+    //         }}
+    //       >
+    //         <a.div
+    //           style={{ height, textTransform: "capitalize" }}
+    //         >
+    //           {items[index]}
+    //         </a.div>
+    //       </a.div>
+    //     ))}
+    //   </div>
+    // </div>
+   
   );
 }
 
---
+++ src/pages/page1/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SoPage =()=>{
+return (
+    <>
+        <div className='so-wrapper'>
+            <div className='so-text-box'>
+            <div id='so-text1'><span className='so-text-enter'>S</span><span className='so-text-delay'>oft</span>  </div>
+            <div id='so-text2'><span className='so-text-enter'>O</span><span className='so-text-delay'>rigin</span></div>
+            </div>
+        </div>
+    </>
+)
+}
+
+export default SoPage;
