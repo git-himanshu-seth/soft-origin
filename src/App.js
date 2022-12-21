676273d08f168a0c6027a4ac262760dbe68d9832
Dipti Patel <diptippatel@gmail.com>
2022-12-21 12:22:47 +0530

    animation for so

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { useTrail, animated as a } from "react-spring";
+import "./App.css";
+
+const items = ["Soft", "Origin"];
+const config = { mass: 9, tension: 100, friction: 400 };
 
 function App() {
+  const [toggle, set] = useState(true);
+  const trail = useTrail(items.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    Y: toggle ? 0 : 0,
+    height: toggle ? 300 : 0,
+    from: { opacity: 0, Y: 20, height: 0 }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="trails-main"
+      onClick={() => set((state) => !state)}
+    >
+      <div>
+        {trail.map(({ Y, height, ...rest }, index) => (
+          <a.div
+            key={items[index]}
+            className="trails-text"
+            style={{
+              ...rest,
+              transform: Y.interpolate((Y) => `translate3d(0,${Y}px,0)`)
+            }}
+          >
+            <a.div
+              style={{ height, textTransform: "capitalize" }}
+            >
+              {items[index]}
+            </a.div>
+          </a.div>
+        ))}
+      </div>
     </div>
   );
 }
